198dea516c57325002be67b03692700a8e192ca5
aliyun2196464012 <lginxai@gmail.com>
2025-09-19 00:23:14 +0800

    init

--- ok/obeditor1/index.tsx
+++
@@ -1,4 +0,0 @@
-export { ObsidianEditor } from '../../packages/ui/src/components/obeditor/ObsidianLayout';
-export { default as ErrorBoundary } from '@/components/obeditor/ErrorBoundary';
-export { TabBar, Tab, type Tab as TabType } from '../../packages/ui/src/components/obeditor/Tab';
-export { default as Editor } from '../../packages/ui/src/components/obeditor/Editor';
